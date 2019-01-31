41b9408af21c3f830539d2498d940b9c0ba68849
Jack758541 <hwang8885556@gmail.com>
2019-01-31 17:33:46 +0800

    qq

--- javascript-boss/template.js
+++ javascript-boss/template.js
@@ -1,33 +1,36 @@
 //import {bgColor,textColor,textFont} from 'number';
 import * as number from '/number'
-let rm = new Vue({
-    el:'#app',
-    data:{
-        styleApp:{
-            width: '1280px',
-            height: '1372px',
-            background: number.bgColor
 
-        },
-        styleMain:{
-            padding: '80px 85px'
-        },
-        styleFooter:{
-            lineHeight: '18px',
-            background: number.textColor,
-            fontSize: '16px',
-            color:number.bgColor,
-            textAlign: 'right',
-            padding: '8px 85px',
-            margin: 0,
-            fontFamily: number.textFont
-        },
-        styleTitle:{
 
-        },
-        styleEnglish:{
-            fontFamily: number.textFont,
-            fontSize: '24px'
-        }
-    }
-})
+console.log(number.bgColor);
+// let rm = new Vue({
+//     el:'#app',
+//     data:{
+//         styleApp:{
+//             width: '1280px',
+//             height: '1372px',
+//             background: number.bgColor
+
+//         },
+//         styleMain:{
+//             padding: '80px 85px'
+//         },
+//         styleFooter:{
+//             lineHeight: '18px',
+//             background: number.textColor,
+//             fontSize: '16px',
+//             color:number.bgColor,
+//             textAlign: 'right',
+//             padding: '8px 85px',
+//             margin: 0,
+//             fontFamily: number.textFont
+//         },
+//         styleTitle:{
+
+//         },
+//         styleEnglish:{
+//             fontFamily: number.textFont,
+//             fontSize: '24px'
+//         }
+//     }
+// })

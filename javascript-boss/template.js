c86c7245be3818e3a285dbe98a00ad919a82be48
Jack758541 <hwang8885556@gmail.com>
2019-01-31 15:52:58 +0800

    footer

--- javascript-boss/template.js
+++ javascript-boss/template.js
@@ -1,11 +1,20 @@
 let rm = new Vue({
     el:'#app',
     data:{
-        styleObject:{
+        styleApp:{
             width: '1280px',
             height: '1372px',
             background: '#F0F0F0'
 
+        },
+        styleMain:{
+
+        },
+        styleFooter:{
+            height:'34px',
+            background:'#2EB738',
+            fontSize: '16px',
+            color: '#F0F0F0'
         }
     }
 })

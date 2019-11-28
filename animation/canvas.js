abd186fdafe145a877effc110b190cf62da3ab3f
Jack758541 <hwang8885556@gmail.com>
2019-11-28 15:48:26 +0800

    'fix:chip殘影'

--- animation/canvas.js
+++ animation/canvas.js
@@ -1,35 +1,32 @@
 var c = document.getElementById('test');
 var t = c.getContext('2d');
 t.imageSmoothingEnabled = false;
-c.setAttribute('width', window.innerWidth);
-c.setAttribute('height', window.innerHeight);
+c.setAttribute('width', window.innerWidth * window.devicePixelRatio);
+c.setAttribute('height', window.innerHeight * window.devicePixelRatio);
 
 var width = c.width;
 var height = c.height;
 var request; //animation
 var speed = 0; //設定速度
-var start = {'x':100, 'y':900};
 var end = {'x':100, 'y':100};
 var savePic;
 var newX;
 var newY;
+var chip = new Image();
+chip.src = 'B_01.svg';
+
 //canvas 點擊動畫
 function animate(){
-    t.clearRect(0, 0, width, height);
-    //t.putImageData(savePic, 40 ,50);
-    request = requestAnimationFrame(animate);
 
+    request = requestAnimationFrame(animate);
     draw();
 }
 
 function draw(){
-    if(!speed==0)
-        t.putImageData(savePic, newX, newY);
-    var chip = new Image();
-    chip.src = 'B_01.svg';
+    t.clearRect(0, 0, width, height);
 
-    this.x = start.x; //起點的X座標
-    this.y = start.y; //起點的Y座標
+    let x = width * 0.1; //起點的X座標
+    let y = height * 0.85; //起點的Y座標
     
     speed += 0.1;
     
@@ -37,30 +34,23 @@ function draw(){
     
     t.beginPath();
     
-    newX = start.x + (end.x - start.x) * speed;
-    newY = start.y + (end.y - start.y) * speed;
-  
-    //t.arc(newX, newY, 15, 0, Math.PI * 2, false);
-    chip.onload = function(){
-        t.drawImage(chip, newX, newY, 50, 50);
-        savePic = t.getImageData(newX, newY, 50, 50);
-    }
-    //t.stroke();
+    newX = x + (end.x - x) * speed;
+    newY = y + (end.y - y) * speed;
 
+    t.drawImage(chip, newX, newY, 50, 50);
+    
     if(newX === end.x && newY === end.y){
         cancelAnimationFrame(request);
         request = null;
         speed = 0;
-        //t.clearRect(0, 0, width, height);
-        t.drawImage(chip, newX, newY, 50, 50);
     }
 
 }
 
 //moving;
 c.addEventListener('click',function(event){
-    //t.clearRect(0, 0, width, height);
-    end.x = event.clientX;
-    end.y = event.clientY;
+
+    end.x = event.offsetX * window.devicePixelRatio;
+    end.y = event.offsetY * window.devicePixelRatio;
     animate();
 })

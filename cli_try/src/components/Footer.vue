a25aab9cb758ea4b894d9b59425e2e7c7ff0da78
Jack758541 <hwang8885556@gmail.com>
2019-12-16 16:55:05 +0800

    'feat:增加頁首頁尾'

---
+++ cli_try/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="footer">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Footer',
+        props: {
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    .footer{
+        position: absolute;
+        width: 100%;
+        height: 5%;
+        bottom: 0%;
+        background-color: gray;
+    }
+</style>
---
+++ cli_try/src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link class="navbar-brand" to="/">Navbar</router-link>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">about</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'Navbar',
+    props: {
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
